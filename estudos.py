581c49cb613d52ddabaa73697646e311ca05692a
danielaniceto <danielgustavots@hotmail.com>
2022-05-18 17:17:37 -0300

    Inserindo o arquivo de estudos

---
+++ estudos.py
@@ -0,0 +1,14 @@
+tarefa = {'titulo': 'Fazer nada', 'descrição': 'nada mesmo'}
+#print(tarefa['titulo'])
+
+lista = [tarefa, {'titulo': 'Daniel', 'descrição': 'desenvolvedor'}]
+print(lista)
+
+lista.append({'titulo':'Batata'})
+
+for tarefa in lista:
+    print(tarefa)
+
+def soma(x, y):
+    return x + y
+print (soma(10,2))
